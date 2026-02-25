ce7b09d5e63aae9ef07232963ede7c00325c74cc
shanmukatonangi <shanmukatonangi@gmail.com>
2026-02-25 21:47:35 +0530

    25thfeb about operators

---
+++ Day1-Basics-24feb/index.js
@@ -0,0 +1,50 @@
+// let username="Shanmukh "
+// username="Praveen"
+
+let favouritenumber=23
+
+// const hobby="Cricket"
+
+// hobby="Tennis"
+
+// console.log(hobby)
+
+//let  -> keyword  -> const   -> var
+//username -> variable ->numbers no , symbols , spaces
+//"Shanmukh"  -> value
+
+// console.log(username)
+
+let num1= 10
+let num2=20
+
+let add=num1+num2
+let sub=num1-num2
+let mul=num1*num2
+let div=num1/num2
+
+
+console.log(add,sub,mul,div)
+
+
+let username="Shanmukh jydt75747ecxcx"
+
+let voteeligible=false
+
+let fullname=username
+console.log(voteeligible)
+
+
+let number1="1"
+let number2=8
+let resultadd=number1+number2
+let resultsub=number1/number2
+// console.log(typeof resultadd)
+
+console.log(resultsub)
+
+
+let str1="Shan"
+let str2=10
+let res=str1/str2
+console.log(res)
---
+++ Day2-Operators-25feb/index.js
@@ -0,0 +1,110 @@
+// // let num=2
+
+
+// // num=num+10
+
+// // num +=10
+
+
+// let a=9
+
+// a=a%2
+// a %=2
+
+// let num=2
+
+// num=num **2 
+
+// num **=2
+
+// let num=10
+// num=num+1
+// num+=1
+// num++
+
+// num=num-1
+// num-=1
+// num--
+
+let num1=2
+// let num2=4
+// // let res=num1+num2
+// console.log('num1+num2',num1+num2)
+
+// console.log(num1++)// 2 num=num+1
+// console.log(num1)
+
+
+//++num1 ///first increaseby1 and then print the resultant value
+//num1++ //first print num1 and the increase
+
+
+
+let num=10
+// console.log(num--)10 10-1
+// console.log(num--)9 9-1
+// console.log(num)8
+
+
+console.log(--num)
+
+// console.log(num++) //10 =10++  =11
+// console.log(++num)  //11
+
+// num++
+
+
+// num++
+// 10
+
+// ++num 10
+
+
+// let shan=10
+// shan++
+// console(shan)
+
+let n1=10
+let n2="10"
+
+// n2 == n1
+// console.log(n2 === n1)
+
+// console.log(10 !== 12)
+
+// console.log(1==1 && 2>2)//false
+
+// console.log(1!=1 || 12==14)//false
+
+// ==
+// ===
+
+// let username="shanmukh"
+// let surname="praveen"
+// let fullname=username+": _ :"+surname
+
+// //username + space + surname
+// console.log(fullname)
+
+
+
+// let age=10
+
+// if(age > 18){
+//     console.log("yes you are eligible")
+// } 
+// else {
+//     console.log("no you are ot eligible")
+// }
+
+
+let userage=10
+console.log(userage>18 ? "Yes" : "No")
+
+
+//if user age is graeter than 18 the we are priniting he is eigible else we are printing he is not eligible
+
+
+let ran=true
+
+console.log(typeof ran)
