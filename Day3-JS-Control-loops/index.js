1c763bd90dcbee119d8daf529c8ed41ba26fec60
shanmukatonangi <shanmukatonangi@gmail.com>
2026-02-27 21:26:39 +0530

    27th feb - control statements

---
+++ Day3-JS-Control-loops/index.js
@@ -0,0 +1,93 @@
+
+
+//normal if else
+// if(marks >=35){
+//     console.log("PASS")
+// }else{
+//     console.log("Fail")
+// }
+
+//Else if
+// if(marks >= 90){
+//     console("A+")
+// }else if(marks>=80){
+//     console.log("A")
+// }else if(marks>=70){
+//     console.log("B")
+// }else{
+//     console.log("F")
+// }
+
+//nested if else loops
+
+// let marks=40
+
+// if(marks>=35 && marks>90){
+//     console.log("You passed the exam")
+   
+//     if(marks>=90){
+//         console.log("you are the topper")
+//     }else{
+//         console.log("but marks are average")
+//     }
+    
+// }else{
+//     console.log("You Failed")
+// }
+
+//switch
+
+// let day=6
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+    
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     default:
+//         console.log("this is random day")
+// }
+
+//task-> 30> Hot and humid
+//<30 >20  Moderate
+//<10 Cold 
+
+//while loops
+//initialization
+//condition check
+//increement
+
+
+// let i=1;
+
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// for(let j=1; j<=10; j++){
+//     console.log(j)
+// }
+
+let i=1
+
+while(i<=5){
+    if(i==3){
+        console.log("Shanmukh")
+    }else{
+        console.log(i)
+    }
+
+    i++
+}
+
+
+//task print from 1 to 7 but in the places of even numbers print you name
